6232b42149e5c38081b834e10c8fcaa69e9a6b0e
Thompson, Darren (IM&T, Clayton) <Darren.Thompson@csiro.au>
2020-09-28 00:35:33 +0000

    Added module init file and renamed Factory and ImageIO souce files

---
+++ itk-module-init.cmake
@@ -0,0 +1,4 @@
+# append local cmake path
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/cmake")
+
+enable_language(CXX)
---
+++ setup.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+from os import sys
+from glob import glob
+
+try:
+    from skbuild import setup
+except ImportError:
+    print('scikit-build is required to build from source.', file=sys.stderr)
+    print('Please run:', file=sys.stderr)
+    print('', file=sys.stderr)
+    print('  python -m pip install scikit-build')
+    sys.exit(1)
+
+setup(
+    name='itk-hdf5container',
+    version='0.1.0',
+    author='Darren Thompson',
+    author_email='darren.thompson@csiro.au',
+    packages=['itk'],
+    package_dir={'itk': 'itk'},
+    download_url=r'https://bitbucket.csiro.au:7999/ct/hdf5container.git',
+    description=r'HDF5Container',
+    long_description='ITK is an open-source, cross-platform library that provides developers with an extensive suite of software tools for image analysis. Developed through extreme programming methodologies, ITK employs leading-edge algorithms for registering and segmenting multidimensional scientific images.',
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: C++",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Healthcare Industry",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Medical Science Apps.",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Software Development :: Libraries",
+        "Operating System :: Android",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+        "Operating System :: MacOS"
+        ],
+    license='Apache',
+    keywords='ITK InsightToolkit',
+    url=r'https://itk.org/',
+    install_requires=[
+        r'itk>=5.2.0'
+    ],
+    data_files=[
+        ('lib', glob('hdf5container-libs/*')),
+    ]
+    )
