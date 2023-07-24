a0e50d1cc51b8d60721f29c78abf10d96db7f82b
Darren Thompson <thompsod@ansto.gov.au>
2023-07-24 14:34:45 +1000

    Updated minor version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except ImportError:
 
 setup(
     name='itk-hdf5container',
-    version='0.4.0',
+    version='0.5.0',
     author='Darren Thompson',
     author_email='darren.thompson@csiro.au',
     packages=['itk'],
